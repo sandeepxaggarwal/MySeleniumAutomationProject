5c863efeba0babdbc4a8744e6af5bc400ff416ea
Sandeep Aggarwal <sandeep.agarwal@mycom-internal.com>
2017-05-28 10:26:17 +0530

    My First Commit

---
+++ src/test/java/TestNGPackage/Test1.java
@@ -0,0 +1,19 @@
+package TestNGPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void doLogin(){
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://newtours.demoaut.com");
+		driver.findElement(By.name("userName")).sendKeys("tutorial");
+		driver.findElement(By.name("password")).sendKeys("tutorial");
+		driver.close();
+	}
+
+}
